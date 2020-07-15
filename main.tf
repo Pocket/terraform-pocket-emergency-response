14758ce3a878e4650ef92e9dcd58d368506f4fe8
kaiser shahid <kaisershahid@gmail.com>
2020-07-14 20:57:12 -0700

    - added sns topic outputs

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
   services           = var.pagerduty_services != null ? var.pagerduty_services : [ ]
 }
 
-resource "aws_sns_topic" "sns_escalation_levels" {
+resource "aws_sns_topic" "topics" {
   for_each = toset(local.escalation_levels)
   name     = "${local.prefix}-SNS-${title(each.value)}"
   tags     = local.tags
@@ -39,7 +39,7 @@ resource "pagerduty_service_integration" "pagerduty_service" {
 # connects each pagerduty_service to its SNS escalation level
 resource "aws_sns_topic_subscription" "pagerduty_sns" {
   count                           = length(local.services)
-  topic_arn                       = aws_sns_topic[ local.services[ count.index ][ "escalation_level" ] ][ "arn" ]
+  topic_arn                       = aws_sns_topic.topics[ local.services[ count.index ][ "escalation_level" ] ][ "arn" ]
   protocol                        = "https"
   endpoint                        = "https://events.pagerduty.com/integration/${pagerduty_service_integration["pagerduty_service"][count.index]["integration_key"]}/enqueue"
   endpoint_auto_confirms          = local.services[ count.index ]["subscription"][ "auto_confirm" ]
