0d7e63e3b16a997f169fcbcce6dc7edd8d65927e
Christina Harlow <charlow@mozilla.com>
2023-01-23 19:47:03 -0500

    chore(updates): updated module to work with terraform 1.3.x

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ locals {
   prefix = var.name_prefix
   tags   = var.tags
 
-  services          = var.pagerduty
-  integrations      = var.pagerduty_integrations != null ? var.pagerduty_integrations : [ ]
+  services     = var.pagerduty
+  integrations = var.pagerduty_integrations != null ? var.pagerduty_integrations : []
 }
 
 resource "aws_sns_topic" "topics" {
@@ -15,14 +15,14 @@ resource "aws_sns_topic" "topics" {
 resource "pagerduty_service" "pagerduty" {
   for_each                = local.services
   name                    = "${local.prefix}-PagerDuty-${title(each.key)}"
-  auto_resolve_timeout    = each.value[ "auto_resolve_timeout" ]
-  acknowledgement_timeout = each.value[ "acknowledgement_timeout" ]
-  alert_creation          = each.value[ "alert_action" ]
-  escalation_policy       = each.value[ "escalation_policy" ]
+  auto_resolve_timeout    = each.value["auto_resolve_timeout"]
+  acknowledgement_timeout = each.value["acknowledgement_timeout"]
+  alert_creation          = each.value["alert_action"]
+  escalation_policy       = each.value["escalation_policy"]
 
   incident_urgency_rule {
-    type    = each.value[ "rule" ][ "type" ]
-    urgency = each.value[ "rule" ][ "urgency" ]
+    type    = each.value["rule"]["type"]
+    urgency = each.value["rule"]["urgency"]
   }
 }
 
@@ -30,17 +30,17 @@ resource "pagerduty_service" "pagerduty" {
 # SNS subscriptions can use to hit PagerDuty
 resource "pagerduty_service_integration" "integrations" {
   count   = length(local.integrations)
-  name    = local.integrations[ count.index ][ "vendor_name" ]
-  vendor  = local.integrations[ count.index ][ "vendor_id" ]
-  service = pagerduty_service.pagerduty[ local.integrations[ count.index ][ "escalation_level" ] ][ "id" ]
+  name    = local.integrations[count.index]["vendor_name"]
+  vendor  = local.integrations[count.index]["vendor_id"]
+  service = pagerduty_service.pagerduty[local.integrations[count.index]["escalation_level"]]["id"]
 }
 
 # connects each pagerduty_service to its SNS escalation level
 resource "aws_sns_topic_subscription" "pagerduty_subscriptions" {
   count                           = length(local.integrations)
-  topic_arn                       = aws_sns_topic.topics[ local.integrations[ count.index ][ "escalation_level" ] ][ "arn" ]
+  topic_arn                       = aws_sns_topic.topics[local.integrations[count.index]["escalation_level"]]["arn"]
   protocol                        = "https"
   endpoint                        = "https://events.pagerduty.com/integration/${pagerduty_service_integration.integrations[count.index]["integration_key"]}/enqueue"
-  endpoint_auto_confirms          = local.integrations[ count.index ][ "subscription" ][ "auto_confirm" ]
-  confirmation_timeout_in_minutes = local.integrations[ count.index ][ "subscription" ][ "confirm_timeout" ]
+  endpoint_auto_confirms          = local.integrations[count.index]["subscription"]["auto_confirm"]
+  confirmation_timeout_in_minutes = local.integrations[count.index]["subscription"]["confirm_timeout"]
 }
